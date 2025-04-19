a9e3ad91eb7c9c09ebeeb47b8daec92ff479faed
chaithanya-venkat <chaithanyavenkat@outlook.com>
2025-04-19 23:27:56 +0530

    100 days of code - Day 1 - Band Name Generator
    This is a python project which takes the user's city name and pet name and concatenates the strings to generate a band name

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("welcome to the band name generator")
+city = input("which city did you grow up in?\n")
+petName = input("what is the name of your pet?\n")
+print("Your band name could be: " + city + " " + petName)
